997bbcea0a41e6b4d2d74e0f162f48b60e025516
Navid Kamali <navidkamali@gmail.com>
2012-06-14 22:01:38 -0700

    Implementing basic all YouTube Standard APIs
     - API includes top rated, top favorites, most viewed, most shared, most popular, most recent, most discussed, most responded, recently featured, trending videos.
     - Adding specs

--- lib/youtube_pop/standard_api.rb
+++ lib/youtube_pop/standard_api.rb
@@ -1,11 +1,30 @@
 require 'youtube_pop/version'
 require 'faraday'
-require 'hpricot'
+require 'nokogiri'
 
 module YoutubePop
+  # Base error class for the extension. Credit to gem youtube_it
+  class Error < RuntimeError
+    attr_reader :code
+    def initialize(msg, code = 0)
+      super(msg)
+      @code = code
+    end
+  end
+
+  class Entry
+    attr_accessor :published, :updated, :content, :link, :author_name
+  end
+
   class StandardApi
     def initialize
-      @conn = Faraday.new(:url => 'https://gdata.youtube.com') do |builder|
+      @entries = []
+
+      options = {
+        :timeout      => 2,
+        :open_timeout => 2
+      }
+      @conn = Faraday.new(:url => 'https://gdata.youtube.com', :options => options) do |builder|
         builder.request  :url_encoded
         builder.response :logger
         builder.adapter  :net_http
@@ -21,10 +40,212 @@ module YoutubePop
     #feed/entry/author/uri
     def top_rated
       response = @conn.get '/feeds/api/standardfeeds/top_rated/'
-      doc = Hpricot(response.body)
-      #doc.search("//feed//entry").each do |entry|
-      #  puts "PUBLISHED: #{(entry/'published')}"
-      #end
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
+    end
+
+    def top_favorites
+      response = @conn.get '/feeds/api/standardfeeds/top_favorites'
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
+    end
+
+    def most_shared
+      response = @conn.get '/feeds/api/standardfeeds/most_shared'
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
+    end
+
+    def most_viewed
+      response = @conn.get '/feeds/api/standardfeeds/most_viewed'
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
+    end
+
+    def most_popular
+      response = @conn.get '/feeds/api/standardfeeds/most_popular'
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
+    end
+
+    def most_recent
+      response = @conn.get '/feeds/api/standardfeeds/most_recent'
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
+    end
+
+    def most_discussed
+      response = @conn.get '/feeds/api/standardfeeds/most_discussed'
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
+    end
+
+    def most_responded
+      response = @conn.get '/feeds/api/standardfeeds/most_responded'
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
+    end
+
+    def recently_featured
+      response = @conn.get '/feeds/api/standardfeeds/recently_featured'
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
+    end
+
+    def trending_videos
+      response = @conn.get '/feeds/api/standardfeeds/on_the_web'
+      doc = Nokogiri::XML(response.body)
+      doc.css('entry').map do |entry|
+        published   = entry.css('published').text
+        updated     = entry.css('updated').text
+        content     = entry.css('content').text
+        link        = entry.at_css("link")['href']
+        author_name = entry.at_css("author uri").text
+
+        entry = Entry.new
+        entry.published = published
+        entry.updated   = updated
+        entry.content   = content
+        entry.link      = link
+        entry.author_name = author_name
+        @entries << entry
+      end
+      @entries
     end
   end
 end
--- spec/youtube_pop_spec.rb
+++ spec/youtube_pop_spec.rb
@@ -5,9 +5,203 @@ describe YoutubePop::StandardApi do
     @youtube_pop = YoutubePop::StandardApi.new
   end
 
-  it "should get top rated data" do
-    #@youtube_pop.top_rated.should == ""
-    puts "RESULTS: #{@youtube_pop.inspect}"
-    puts "RESULTS: #{@youtube_pop.top_rated}"
+  describe '#top_rated' do
+    before do
+      @entries = @youtube_pop.top_rated
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
+  end
+
+  describe '#top_favorites' do
+    before do
+      @entries = @youtube_pop.top_favorites
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
+  end
+
+  describe '#most_viewed' do
+    before do
+      @entries = @youtube_pop.most_viewed
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
+  end
+
+  describe '#most_shared' do
+    before do
+      @entries = @youtube_pop.most_shared
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
+  end
+
+  describe '#most_popular' do
+    before do
+      @entries = @youtube_pop.most_popular
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
+  end
+
+  describe '#most_recent' do
+    before do
+      @entries = @youtube_pop.most_recent
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
+  end
+
+  describe '#most_discussed' do
+    before do
+      @entries = @youtube_pop.most_discussed
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
+  end
+
+  describe '#most_responded' do
+    before do
+      @entries = @youtube_pop.most_responded
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
+  end
+
+  describe '#recently_featured' do
+    before do
+      @entries = @youtube_pop.recently_featured
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
+  end
+
+  describe '#trending_videos' do
+    before do
+      @entries = @youtube_pop.trending_videos
+    end
+
+    it "should get data" do
+      @entries.size.should > 0
+    end
+
+    it "should contain valid data" do
+      @entries.each do |entry|
+        entry.should be_a_kind_of(YoutubePop::Entry)
+        entry.published.should  =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.updated.should    =~ /^[\d]{4}\-[\d]{2}\-[\w]{5}:[\d]{2}:[\d]{2}\.[\w]{4}/
+        entry.link.should start_with 'https://'
+        entry.author_name.should_not be_empty
+      end
+    end
   end
 end
