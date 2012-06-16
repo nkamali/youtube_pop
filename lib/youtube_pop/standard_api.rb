67c0411f07f7e60ad6b64919d02b9480bc771774
Navid Kamali <navidkamali@gmail.com>
2012-06-16 07:14:34 -0700

    DRYing up and bumping up version

--- lib/youtube_pop/standard_api.rb
+++ lib/youtube_pop/standard_api.rb
@@ -16,6 +16,11 @@ module YoutubePop
     attr_accessor :published, :updated, :content, :link, :author_name
   end
 
+  # Searches for the following from the response:
+  #
+  # feed/entry/published, feed/entry/updated
+  # feed/entry/content, feed/entry/link rel="alternate" href=
+  # feed/entry/author/name
   class StandardApi
     def initialize
       @entries = []
@@ -31,204 +36,59 @@ module YoutubePop
       end
     end
 
-    #feed/entry/published
-    #feed/entry/updated
-    #feed/entry/content
-    #feed/entry/link rel="alternate" href=
-    #
-    #feed/entry/author/name
-    #feed/entry/author/uri
     def top_rated
       response = @conn.get '/feeds/api/standardfeeds/top_rated/'
-      doc = Nokogiri::XML(response.body)
-      doc.css('entry').map do |entry|
-        published   = entry.css('published').text
-        updated     = entry.css('updated').text
-        content     = entry.css('content').text
-        link        = entry.at_css("link")['href']
-        author_name = entry.at_css("author uri").text
-
-        entry = Entry.new
-        entry.published = published
-        entry.updated   = updated
-        entry.content   = content
-        entry.link      = link
-        entry.author_name = author_name
-        @entries << entry
-      end
-      @entries
+      @entries = get_entries(response)
     end
 
     def top_favorites
       response = @conn.get '/feeds/api/standardfeeds/top_favorites'
-      doc = Nokogiri::XML(response.body)
-      doc.css('entry').map do |entry|
-        published   = entry.css('published').text
-        updated     = entry.css('updated').text
-        content     = entry.css('content').text
-        link        = entry.at_css("link")['href']
-        author_name = entry.at_css("author uri").text
-
-        entry = Entry.new
-        entry.published = published
-        entry.updated   = updated
-        entry.content   = content
-        entry.link      = link
-        entry.author_name = author_name
-        @entries << entry
-      end
-      @entries
+      @entries = get_entries(response)
     end
 
     def most_shared
       response = @conn.get '/feeds/api/standardfeeds/most_shared'
-      doc = Nokogiri::XML(response.body)
-      doc.css('entry').map do |entry|
-        published   = entry.css('published').text
-        updated     = entry.css('updated').text
-        content     = entry.css('content').text
-        link        = entry.at_css("link")['href']
-        author_name = entry.at_css("author uri").text
-
-        entry = Entry.new
-        entry.published = published
-        entry.updated   = updated
-        entry.content   = content
-        entry.link      = link
-        entry.author_name = author_name
-        @entries << entry
-      end
-      @entries
+      @entries = get_entries(response)
     end
 
     def most_viewed
       response = @conn.get '/feeds/api/standardfeeds/most_viewed'
-      doc = Nokogiri::XML(response.body)
-      doc.css('entry').map do |entry|
-        published   = entry.css('published').text
-        updated     = entry.css('updated').text
-        content     = entry.css('content').text
-        link        = entry.at_css("link")['href']
-        author_name = entry.at_css("author uri").text
-
-        entry = Entry.new
-        entry.published = published
-        entry.updated   = updated
-        entry.content   = content
-        entry.link      = link
-        entry.author_name = author_name
-        @entries << entry
-      end
-      @entries
+      @entries = get_entries(response)
     end
 
     def most_popular
       response = @conn.get '/feeds/api/standardfeeds/most_popular'
-      doc = Nokogiri::XML(response.body)
-      doc.css('entry').map do |entry|
-        published   = entry.css('published').text
-        updated     = entry.css('updated').text
-        content     = entry.css('content').text
-        link        = entry.at_css("link")['href']
-        author_name = entry.at_css("author uri").text
-
-        entry = Entry.new
-        entry.published = published
-        entry.updated   = updated
-        entry.content   = content
-        entry.link      = link
-        entry.author_name = author_name
-        @entries << entry
-      end
-      @entries
+      @entries = get_entries(response)
     end
 
     def most_recent
       response = @conn.get '/feeds/api/standardfeeds/most_recent'
-      doc = Nokogiri::XML(response.body)
-      doc.css('entry').map do |entry|
-        published   = entry.css('published').text
-        updated     = entry.css('updated').text
-        content     = entry.css('content').text
-        link        = entry.at_css("link")['href']
-        author_name = entry.at_css("author uri").text
-
-        entry = Entry.new
-        entry.published = published
-        entry.updated   = updated
-        entry.content   = content
-        entry.link      = link
-        entry.author_name = author_name
-        @entries << entry
-      end
-      @entries
+      @entries = get_entries(response)
     end
 
     def most_discussed
       response = @conn.get '/feeds/api/standardfeeds/most_discussed'
-      doc = Nokogiri::XML(response.body)
-      doc.css('entry').map do |entry|
-        published   = entry.css('published').text
-        updated     = entry.css('updated').text
-        content     = entry.css('content').text
-        link        = entry.at_css("link")['href']
-        author_name = entry.at_css("author uri").text
-
-        entry = Entry.new
-        entry.published = published
-        entry.updated   = updated
-        entry.content   = content
-        entry.link      = link
-        entry.author_name = author_name
-        @entries << entry
-      end
-      @entries
+      @entries = get_entries(response)
     end
 
     def most_responded
       response = @conn.get '/feeds/api/standardfeeds/most_responded'
-      doc = Nokogiri::XML(response.body)
-      doc.css('entry').map do |entry|
-        published   = entry.css('published').text
-        updated     = entry.css('updated').text
-        content     = entry.css('content').text
-        link        = entry.at_css("link")['href']
-        author_name = entry.at_css("author uri").text
-
-        entry = Entry.new
-        entry.published = published
-        entry.updated   = updated
-        entry.content   = content
-        entry.link      = link
-        entry.author_name = author_name
-        @entries << entry
-      end
-      @entries
+      @entries = get_entries(response)
     end
 
     def recently_featured
       response = @conn.get '/feeds/api/standardfeeds/recently_featured'
-      doc = Nokogiri::XML(response.body)
-      doc.css('entry').map do |entry|
-        published   = entry.css('published').text
-        updated     = entry.css('updated').text
-        content     = entry.css('content').text
-        link        = entry.at_css("link")['href']
-        author_name = entry.at_css("author uri").text
-
-        entry = Entry.new
-        entry.published = published
-        entry.updated   = updated
-        entry.content   = content
-        entry.link      = link
-        entry.author_name = author_name
-        @entries << entry
-      end
-      @entries
+      @entries = get_entries(response)
     end
 
     def trending_videos
       response = @conn.get '/feeds/api/standardfeeds/on_the_web'
+      @entries = get_entries(response)
+    end
+
+    private
+
+    def get_entries(response)
       doc = Nokogiri::XML(response.body)
       doc.css('entry').map do |entry|
         published   = entry.css('published').text
